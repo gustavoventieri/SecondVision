bb33e3238211a4326c265bdb1e51d7b63ba4a4ee
gustavoventieri <ventierigustavo@gmail.com>
2024-07-29 03:40:47 -0300

    feat: icon photo updated

---
+++ mobile/App.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "react-native-gesture-handler";
+import { enableScreens } from "react-native-screens";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import BluetoothOffScreen from "./src/screens/BluetoothOff";
+import BluetoothOnScreen from "./src/screens/BluetoothOn";
+import Home from "./src/screens/Home";
+const Stack = createStackNavigator();
+
+enableScreens();
+export default function App() {
+  return (
+    <NavigationContainer independent={true}>
+      <Stack.Navigator initialRouteName="LoadingScreen">
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }}
+        />
+
+        <Stack.Screen
+          name="BluetoothOn"
+          component={BluetoothOnScreen}
+          options={{ headerShown: false }}
+        />
+
+        <Stack.Screen
+          name="BluetoothOff"
+          component={BluetoothOffScreen}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ mobile/src/screens/Home.tsx
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from "react";
+import { View, StyleSheet, Dimensions } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import * as Font from "expo-font";
+
+import { About } from "../components/About";
+import { Header } from "../components/Header";
+import { Devices } from "../components/Devices";
+import { Dashboard } from "../components/Dashboard";
+
+const loadFonts = async () => {
+  await Font.loadAsync({
+    FonteCustomizada: require("../../assets/fonts/Poppins-SemiBoldItalic.ttf"),
+  });
+};
+
+export default function Home() {
+  const [isOn, setIsOn] = useState(false);
+  const [StatusText, setStatusText] = useState("Desligado");
+
+  const toggleSwitch = () => {
+    setIsOn(!isOn);
+    setStatusText(isOn ? "Desligado" : "Ligado");
+  };
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  useEffect(() => {
+    loadFonts().then(() => setFontsLoaded(true));
+  }, []);
+
+  if (!fontsLoaded) {
+    return null; // Ou um componente de carregamento
+  }
+
+  return (
+    <View style={styles.container}>
+      <LinearGradient
+        // Background Linear Gradient
+        colors={["#45A7FF", "#001268"]}
+        style={styles.background}
+      />
+      <Header toggleMenu={toggleMenu} props="Second Vision" />
+      <Devices />
+      <Dashboard
+        toggleSwitch={toggleSwitch}
+        isOn={isOn}
+        statusText={StatusText}
+      />
+      <About visible={isMenuOpen} onClose={() => setIsMenuOpen(false)} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-between",
+  },
+  background: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    top: 0,
+    height: "100%",
+  },
+});
