ca489b2f554aa75f6ae40311822f298c0bc02dc8
nickolss <nickolasmaiaraujo@gmail.com>
2024-08-31 08:36:23 -0300

    feat: Modified navigator logic

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,45 +1,88 @@
-import React from "react";
-import "react-native-gesture-handler";
-import { enableScreens } from "react-native-screens";
+import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./src/screens/Home";
+import SettingsScreen from "./src/screens/Settings";
+import ControlScreen from "./src/screens/ControlScreen";
 import BluetoothOffScreen from "./src/screens/BluetoothOff";
 import BluetoothOnScreen from "./src/screens/BluetoothOn";
-import Home from "./src/screens/Home";
-import ControlScreen from "./src/screens/ControlScreen";
+import IntervalScreen from "./src/screens/Interval";
+import OperationModeScreen from "./src/screens/OperationMode";
+import { Ionicons } from "@expo/vector-icons";
 
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
 
+function TabNavigator() {
+  return (
+    <Tab.Navigator initialRouteName="ControlScreen">
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home-outline" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={SettingsScreen}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="settings-outline" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
 
-const Stack = createStackNavigator();
+function AppNavigator() {
+  return (
+    <Stack.Navigator initialRouteName="TabNavigator">
+      <Stack.Screen
+        name="TabNavigator"
+        component={TabNavigator}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="ControlScreen"
+        component={ControlScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="BluetoothOff"
+        component={BluetoothOffScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="BluetoothOn"
+        component={BluetoothOnScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Interval"
+        component={IntervalScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="OperationMode"
+        component={OperationModeScreen}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
+  );
+}
 
-enableScreens();
+// Componente principal
 export default function App() {
   return (
-    <NavigationContainer independent={true}>
-      <Stack.Navigator initialRouteName="ControlScreen">
-        <Stack.Screen
-          name="ControlScreen"
-          component={ControlScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="BluetoothOff"
-          component={BluetoothOffScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="BluetoothOn"
-          component={BluetoothOnScreen}
-          options={{
-            headerShown: false,
-          }}
-        />
-      </Stack.Navigator>
+    <NavigationContainer>
+      <AppNavigator />
     </NavigationContainer>
   );
 }
