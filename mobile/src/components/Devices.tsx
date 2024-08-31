0ddec64a0721bead96230c2ae727c5152b6971e1
nickolss <nickolasmaiaraujo@gmail.com>
2024-08-31 08:37:27 -0300

    style: Restyled pages

--- mobile/src/components/Devices.tsx
+++ mobile/src/components/Devices.tsx
@@ -1,64 +1,35 @@
 import React from "react";
 import { useNavigation, useRoute } from "@react-navigation/native";
-import { View, Image, Dimensions, StyleSheet,TouchableHighlight } from "react-native";
+import {
+	View,
+	Image,
+	Dimensions,
+	StyleSheet,
+	TouchableHighlight,
+} from "react-native";
 const { width } = Dimensions.get("window");
 export function Devices() {
-  const route = useRoute();
+	const route = useRoute();
 
-  if (route.name == "BluetoothOn") {
-    return (
-      <View style={styles.titulo}>
-        <Image
-          source={require("../../assets/images/Logo.png")}
-          style={styles.image}
-          resizeMode="contain"
-        />
-
-        
-      </View>
-    );
-  } else {
-    return (
-      <View style={styles.titulo}>
-        <Image
-          source={require("../../assets/images/Logo.png")}
-          style={styles.image}
-          resizeMode="contain"
-        />
-
-        <View style={styles.dispositivo}>
-          <Image
-            source={require("../../assets/images/Logo.png")}
-            style={styles.image}
-            resizeMode="contain"
-          />
-        </View>
-      </View>
-    );
-  }
+	return (
+		<View style={styles.titulo}>
+			<Image
+				source={require("../../assets/images/Logo.png")}
+				style={styles.image}
+				resizeMode="contain"
+			/>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  image: {
-    width: "60%", 
-    height: undefined,
-    aspectRatio: 1.3,
-
-   
-  },
-  titulo: {
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  dispositivo: {
-    width: width * 0.5, 
-    height: width * 0.5, 
-    borderRadius: (width * 0.5) / 2, 
-    backgroundColor: "white", 
-    justifyContent: "center",
-    alignItems: "center",
-    bottom: 20,
-  },
-
-  
+	image: {
+		width: "60%",
+		height: undefined,
+		aspectRatio: 1.3,
+	},
+	titulo: {
+		justifyContent: "center",
+		alignItems: "center",
+	},
 });
--- mobile/src/components/Header.tsx
+++ mobile/src/components/Header.tsx
@@ -1,67 +1,75 @@
 import React from "react";
-import {
-  Text,
-  View,
-  Pressable,
-  StyleSheet,
-  Dimensions,
-} from "react-native";
+import { Text, View, Pressable, StyleSheet, Dimensions } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { useNavigation, useRoute } from "@react-navigation/native";
 
 const { width } = Dimensions.get("window");
 
 interface HeaderProps {
-  toggleMenu: () => void;
-  props: string;
-  sendShutdownCommand: () => void;
+	toggleMenu: () => void;
+	props: string;
 }
 
-export const Header: React.FC<HeaderProps> = ({ toggleMenu, props, sendShutdownCommand }) => {
-  const navigation = useNavigation();
-  const route = useRoute();
+export const Header: React.FC<HeaderProps> = ({
+	toggleMenu,
+	props,
+}) => {
+	const navigation = useNavigation();
+	const route = useRoute();
 
-  if (route.name == "BluetoothOn") {
-    return (
-      <View style={styles.headerOptions}>
-        <Pressable onPress={toggleMenu}>
-          <Ionicons name="information-circle-outline" size={35} color="white" />
-        </Pressable>
-        <Text style={styles.textFontOn}>{props}</Text>
-      </View>
-    );
-  } else {
-    return (
-      <View style={styles.headerOptions}>
-        <Pressable onPress={toggleMenu}>
-          <Ionicons name="information-circle-outline" size={35} color="white" />
-        </Pressable>
-        <Text style={styles.textFont}>{props}</Text>
-        <Pressable onPress={sendShutdownCommand}>
-          <Ionicons name="power" size={32} color="white" />
-        </Pressable>
-      </View>
-    );
-  }
+	if (route.name == "BluetoothOn") {
+		return (
+			<View style={styles.headerOptions}>
+				<Pressable onPress={toggleMenu} style={styles.information}>
+					<Ionicons
+						name="information-circle-outline"
+						size={35}
+						color="#001268"
+					/>
+				</Pressable>
+				<Text style={styles.textFontOn}>{props}</Text>
+			</View>
+		);
+	} else {
+		return (
+			<View style={styles.headerOptions}>
+				<Pressable onPress={toggleMenu} style={styles.information}>
+					<Ionicons
+						name="information-circle-outline"
+						size={35}
+						color="#001268"
+            style={styles.information2}
+					/>
+				</Pressable>
+				<Text style={styles.textFont}>{props}</Text>
+			</View>
+		);
+	}
 };
 
 const styles = StyleSheet.create({
-  headerOptions: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    padding: 10,
-    top: 5,
-  },
-  textFont: {
-    fontWeight: "bold",
-    fontSize: width * 0.05,
-    color: "white",
-  },
-  textFontOn: {
-    fontWeight: "bold",
-    fontSize: width * 0.05,
-    color: "white",
-    right: width * 0.27,
-  },
+	headerOptions: {
+		flexDirection: "row",
+		alignItems: "center",
+		padding: 10,
+		top: 5,
+	},
+	information: {
+		alignSelf: 'flex-start',
+	},
+	information2: {
+		alignSelf: 'flex-start',
+	},
+	textFont: {
+    margin: 'auto',
+		fontWeight: "bold",
+		fontSize: width * 0.05,
+		color: "#001268",
+	},
+	textFontOn: {
+    margin: 'auto',
+		fontWeight: "bold",
+		fontSize: width * 0.05,
+		color: "#001268",
+	},
 });
